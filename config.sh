fca6ec940a84691cef2176562c5216c62766e13d
matthesvoss <matthesvoss7@gmail.com>
2015-11-17 14:28:43 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,11 +2,12 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-#ipython
+#python
 sudo apt-get install -y libzmq-dev
 sudo apt-get install -y python-dev
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 rm get-pip.py
 
-
+# Just To Install weasyprint
+sudo apt-get install python-dev python-pip python-lxml libcairo2 libpango1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
